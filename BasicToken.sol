5bd0b9cf939143c38c0eba724d3bbd960507a4a2
BGG Lending Coin <33961227+goldgate@users.noreply.github.com>
2017-11-24 22:11:20 +0700

    Create BasicToken.sol

---
+++ BasicToken.sol
@@ -0,0 +1,30 @@
+/**
+ * @title Basic token
+ * Basic version of StandardToken, with no allowances. 
+ */
+contract BasicToken is ERC20Basic {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) balances;
+
+  /**
+  * transfer token for a specified address
+  */
+  function transfer(address _to, uint256 _value) returns (bool) {
+    require(_to != address(0));
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+  * Gets the balance of the specified address.
+  */
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
